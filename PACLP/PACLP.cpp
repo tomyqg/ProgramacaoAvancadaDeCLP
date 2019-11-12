46a8d1a6a473992acf51786a4d9b1ad399e9f11e
Caio Gonçalves <goncalves.costa@live.com>
2019-11-11 21:36:50 -0300

    Adição de bibliotecas e criação de solution

---
+++ PACLP/PACLP.cpp
@@ -0,0 +1,31 @@
+// PACLP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "modbusLib/modbus.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+	/*modbus_t* ctx;
+
+	ctx = modbus_new_tcp("127.0.0.1", 502);
+	if (modbus_connect(ctx) == -1) {
+		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+		modbus_free(ctx);
+		return -1;
+	}*/
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
