ec0cd0eae8097041cfc2cb93352b7c072ac5c780
Caio Gonçalves <goncalves.costa@live.com>
2019-11-23 21:04:02 -0300

    Programa que lê e escreve no simulador de CLP

--- PACLP/PACLP.cpp
+++ PACLP/PACLP.cpp
@@ -1,96 +1,72 @@
-// PACLP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
-/*
-int main()
-{
-    std::cout << "Hello World!\n";
+//#include "libmodbus/src/modbus.h"
 
-	modbus_t* ctx;
-
-	ctx = modbus_new_tcp("127.0.0.1", 502);
-	if (modbus_connect(ctx) == -1) {
-		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-		modbus_free(ctx);
-		return -1;
-	}
-
-}
-*/
-
-
-
-/*
- * Copyright © 2008-2014 Stéphane Raimbault <stephane.raimbault@gmail.com>
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
 
 #include <stdio.h>
 #ifndef _MSC_VER
 #include <unistd.h>
+#include <sys/time.h>
 #endif
+#include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include <errno.h>
+#include "libmodbus/src/modbus.h"
+void test() {
+	//Create a new RTU context with proper serial parameters (in this example,
+	//device name /dev/ttyS0, baud rate 9600, no parity bit, 8 data bits, 1 stop bit)
 
-#include "modbusLib/modbus.h"
+	char serialPort[5] = "COM1";
+	const int numberOfRegistersToRead = 25;
+	uint16_t reg[numberOfRegistersToRead];// will store read registers values
 
-int main(void)
-{
-	int s = -1;
-	modbus_t* ctx;
-	modbus_mapping_t* mb_mapping;
+	int registerAddrToWrite = 1;
+	uint16_t valueToWriteToRegister = 999;
 
-	ctx = modbus_new_tcp("127.0.0.1", 1502);
-	/* modbus_set_debug(ctx, TRUE); */
+	modbus_t* ctx = modbus_new_rtu(serialPort, 9600, 'N', 8, 1);
+	if (!ctx) {
+		fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
+		exit(1);
+	}
 
-	mb_mapping = modbus_mapping_new(500, 500, 500, 500);
-	if (mb_mapping == NULL) {
-		fprintf(stderr, "Failed to allocate the mapping: %s\n",
-			modbus_strerror(errno));
+	if (modbus_connect(ctx) == -1) {
+		fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
 		modbus_free(ctx);
-		return -1;
+		exit(1);
 	}
 
-	s = modbus_tcp_listen(ctx, 1);
-	modbus_tcp_accept(ctx, &s);
+	//Set the Modbus address of the remote slave (to 3)
+	modbus_set_slave(ctx, 3);
 
-	for (;;) {
-		uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
-		int rc;
 
-		rc = modbus_receive(ctx, query);
-		if (rc > 0) {
-			/* rc is the query size */
-			modbus_reply(ctx, query, rc, mb_mapping);
+	
+
+	//read registers
+	int num = modbus_read_registers(ctx, 1, numberOfRegistersToRead, reg);
+	if (num != numberOfRegistersToRead) {// number of read registers is not the one expected
+		fprintf(stderr, "Failed to read: %s\n", modbus_strerror(errno));
+	}
+	else {
+		for (int i = 0; i < numberOfRegistersToRead; i++) {
+			printf("\nRegister(%d): %d",i+1, reg[i]);
 		}
-		else if (rc == -1) {
-			/* Connection closed by the client or error */
-			break;
-		}
 	}
 
-	printf("Quit the loop: %s\n", modbus_strerror(errno));
 
-	if (s != -1) {
-		//close(s);
-	}
-	modbus_mapping_free(mb_mapping);
+	//write registers
+	modbus_write_register(ctx, registerAddrToWrite, valueToWriteToRegister);
+
+
+
 	modbus_close(ctx);
 	modbus_free(ctx);
-
-	return 0;
 }
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+int main(/*int argc, char* argv[]*/)
+{
+	test();
+
+}
