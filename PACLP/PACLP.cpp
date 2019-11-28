85f632aed57f6ff95ec39be44a7e19713558d9ac
Caio Gonçalves <goncalves.costa@live.com>
2019-11-27 22:49:49 -0300

    .

--- PACLP/PACLP.cpp
+++ PACLP/PACLP.cpp
@@ -1,7 +1,3 @@
-
-//#include "libmodbus/src/modbus.h"
-
-
 #include <stdio.h>
 #ifndef _MSC_VER
 #include <unistd.h>
@@ -13,13 +9,14 @@
 #include <errno.h>
 #include "libmodbus/src/modbus.h"
 #include <iostream>
-void test() {
-	//Create a new RTU context with proper serial parameters (in this example,
-//device name /dev/ttyS0, baud rate 9600, no parity bit, 8 data bits, 1 stop bit)
-	//char serialPort[9] = "\\\\.\\COM2";
-	char serialPort[5] = "COM7";
 
-	modbus_t* ctx = modbus_new_rtu(serialPort, 9600, 'N', 8, 1);
+
+char serialPort[5] = "COM7";
+int baudRate = 9600;
+modbus_t* ctx = NULL;
+
+void setUpSerialConnection() {
+	ctx = modbus_new_rtu(serialPort, baudRate, 'N', 8, 1);
 	if (!ctx) {
 		fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
 		exit(1);
@@ -31,30 +28,37 @@ void test() {
 		exit(1);
 	}
 
-	//Set the Modbus address of the remote slave (to 3)
 	modbus_set_slave(ctx, 1);
+}
 
-	uint16_t reg[5];// will store read registers values
-
-	//Read 5 holding registers starting from address 10
-	int num = modbus_read_registers(ctx, 1, 5, reg);
-  	if (num ==-1) {// number of read registers is not the one expected		
+uint16_t readRegisters(int registerAddr) {
+	uint16_t reg[1];
+	int num = modbus_read_registers(ctx, registerAddr, 1, reg);
+	if (num == -1) {
 		fprintf(stderr, "Failed to read: %s\n", modbus_strerror(errno));
 	}
 
-	modbus_write_register(ctx, 0, 6969);
+	return reg[0];
+}
 
-	std::cout << "\n" << reg[1] << "\n";
+void writeRegister(int registerAddr, uint16_t value) {
+	modbus_write_register(ctx, registerAddr, value);
+}
 
+void closeConnection() {
 	modbus_close(ctx);
 	modbus_free(ctx);
 }
 
-
-
-
-int main(/*int argc, char* argv[]*/)
+int main()
 {
-	test();
+	setUpSerialConnection();
+
+	uint16_t registerRead = readRegisters(0);
+	std::cout << "\n" << registerRead << "\n";
+
+	writeRegister(0, 'L');
+
+	closeConnection();
 
 }
